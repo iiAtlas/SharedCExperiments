8bb856aaeef881b80a8c4ec223cd1eae8e25e436
iiAtlas <agwny94@gmail.com>
2013-02-17 18:56:51 -0500

    Set up basic program

--- experiments.c
+++ experiments.c
@@ -1,6 +1,22 @@
 #include "stdio.h"
 
+void atlas();
+void wally();
+
 int main() {
-	printf("Starting Experimentation...\n");
+	printf("Starting experimentation...\n");
+	
+	atlas();
+	wally();
+
+	printf("Completed experimentation.\n");
 	return 0;
+}
+
+void atlas() {
+	printf("Executing Atlas' experiments...\n");
+}
+
+void wally() {
+	printf("Executing Wally's experiments...\n");
 }
