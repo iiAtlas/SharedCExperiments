50a4c9481a08fe03532e63bcc92ebdfa099f3c25
iiAtlas <agwny94@gmail.com>
2013-02-17 19:18:10 -0500

    Added compile.sh script

--- experiments.c
+++ experiments.c
@@ -3,8 +3,10 @@
 void atlas();
 void wally();
 
+void swapVars(int *v1, int *v2); //Swap values of v1 and v2
+
 int main() {
-	printf("Starting experimentation...\n");
+	printf("Starting experimentation...\n\n");
 	
 	atlas();
 	wally();
@@ -15,8 +17,23 @@ int main() {
 
 void atlas() {
 	printf("Executing Atlas' experiments...\n");
+
+	int myV1 = 10, myV2 = 20;
+	printf("myV1 = %d, myV2 = %d\n", myV1, myV2);
+	swapVars(&myV1, &myV2);
+	printf("myV1 = %d, myV2 = %d\n", myV1, myV2);
+
+	printf("Completed Atlas' experiments.\n\n");
+}
+
+void swapVars(int *v1, int *v2) {
+	int temp = *v1;
+	*v1 = *v2;
+	*v2 = temp;
 }
 
 void wally() {
 	printf("Executing Wally's experiments...\n");
+
+	printf("Completed Wally's experiments.\n\n");
 }
